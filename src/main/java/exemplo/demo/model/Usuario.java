5c834e838f1c0fd26401cdfac6247abcd621043b
Guest 01 <guest01@alunos.sc.senac.br>
2024-10-25 19:48:46 -0300

    primeiro commit

---
+++ src/main/java/exemplo/demo/model/Usuario.java
@@ -0,0 +1,14 @@
+package exemplo.demo.model;
+
+public class Usuario {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
